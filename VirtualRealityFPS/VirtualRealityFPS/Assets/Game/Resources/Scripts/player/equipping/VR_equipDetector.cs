17f2caae4ce51aeba1af33bab2469e238b1e527e
Fergus <ausski101@outlook.com>
2018-11-17 23:51:23 +1000

    commit

--- VirtualRealityFPS/VirtualRealityFPS/Assets/Game/Resources/Scripts/player/equipping/VR_equipDetector.cs
+++ VirtualRealityFPS/VirtualRealityFPS/Assets/Game/Resources/Scripts/player/equipping/VR_equipDetector.cs
@@ -5,13 +5,10 @@ using UnityEngine;
 public class VR_equipDetector : MonoBehaviour {
 
     public Transform selectedObject;
-	[HideInInspector]
-	public  string nameofthegun;
 
     void OnTriggerStay(Collider detectedCollision)
     {
         selectedObject = detectedCollision.gameObject.transform;
-		nameofthegun = detectedCollision.gameObject.name;
 
     }
 }
